c4ee9fc814722002ac482d8a645c7ac0af0da4d5
Cole Brown <coletbrown@icloud.com>
2026-03-24 21:39:48 -0400

    Add files via upload

--- NestPay/next.config.js
+++ NestPay/next.config.js
@@ -1,3 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['stripe'],
+  },
+}
 module.exports = nextConfig
